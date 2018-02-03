3b648eb6d735580253943d8765886e556c5aa5ed
Murali Krishna <gadevenkatamuralikrishna@gmail.com>
2018-02-03 17:37:21 +0530

    Test 2: Adding new controller and files

---
+++ WebApplication1/Controllers/DataTestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/DataTest")]
+    public class DataTestController : Controller
+    {
+        [HttpGet("GetNames")]
+        public IActionResult GetNames()
+        {
+            var list = new List<Hero>
+            {
+                new Hero{HeroName="Iron Man", Comics = "Marvel"},
+                new Hero{HeroName="Superman", Comics = "DC"},
+                new Hero{HeroName="Batman", Comics = "DC"},
+                new Hero{HeroName="Captain America", Comics = "Marvel"},
+                new Hero{HeroName="Thor", Comics = "Marvel"},
+                new Hero{HeroName="Aqua Man", Comics = "DC"},
+
+            };
+            var ob = new JsonResult(list);
+            return Ok(ob.Value);
+        }
+    }
+}
---
+++ WebApplication1/Model/Hero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Hero
+    {
+        public string HeroName { get; set; }
+        public string Comics { get; set; }
+        
+    }
+}
